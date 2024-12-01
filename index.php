90483c17966788f26c89b4fbee63dbdde3669dc5
SanthyagoSouza <santhyagoponciano@gmail.com>
2024-12-01 20:21:41 -0300

    primeiro_commit

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descricao do site">
+    <meta name="keywords" content="palavras,chaves,do,site">
+    <link rel="stylesheet" href="estile/style.css">
+    <title>projeto 01</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="center">
+            <div class="logo left">logomarca</div>
+            <nav class="desktop right">
+                <ul>
+                    <li><a href=" ">Home</a></li>
+                    <li><a href=" ">Sobre</a></li>
+                    <li><a href=" ">Servicos</a></li>
+                    <li><a href=" ">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile right">
+                <ul>
+                    <li><a href=" ">Home</a></li>
+                    <li><a href=" ">Sobre</a></li>
+                    <li><a href=" ">Servicos</a></li>
+                    <li><a href=" ">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div><!--clear-->
+        </div><!--center-->
+    </header>
+    <section class="banner-principal">
+        <div class="center">
+            <form action="get">
+                <h2>Qual o seu melhor email</h2>
+                <input type="email" name="" id="" required>
+                <input type="submit" value="Cadastrar" name="acao">
+            </form>
+
+        </div>
+    </section>
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Santhyago Souza</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptatem voluptas ad deleniti corporis, illum
+                    quidem magni rem minus nisi reprehenderit vel repellat, dolor temporibus eaque
+                    fuga numquam suscipit ipsam
+                    blanditiis.</p>
+            </div><!--w50-->
+            <div class="w50 left">
+                <img src="https://cdn-icons-png.flaticon.com/512/1373/1373255.png" alt="">
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div>
+    </section>
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+                <h3>Css3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse delectus, nobis voluptatum tempore
+                    saepe rerum eos alias id modi laboriosam aliquam voluptatem voluptatibus magni, dolorum ipsam!
+                    Asperiores, accusamus a. Soluta.</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+                <h3>Html5</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse delectus, nobis voluptatum tempore
+                    saepe rerum eos alias id modi laboriosam aliquam voluptatem voluptatibus magni, dolorum ipsam!
+                    Asperiores, accusamus a. Soluta.</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-codepen" aria-hidden="true"></i></h3>
+                <h3>Java script</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse delectus, nobis voluptatum tempore
+                    saepe rerum eos alias id modi laboriosam aliquam voluptatem voluptatibus magni, dolorum ipsam!
+                    Asperiores, accusamus a. Soluta.</p>
+            </div>
+        </div>
+    </section>
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left">
+                <h2 class="title">depoimentos</h2>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+                        consequatur debitis totam sit
+                        rem, sapiente ea delectus veritatis sed. Corrupti magni quam commodi error quae ratione,
+                        deserunt facere nemo? Maxime!</p>
+                    <p class="nome-autor">Lorem ipsum</p>
+                </div>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+                        consequatur debitis totam sit
+                        rem, sapiente ea delectus veritatis sed. Corrupti magni quam commodi error quae ratione,
+                        deserunt facere nemo? Maxime!</p>
+                    <p class="nome-autor">Lorem ipsum</p>
+                </div>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+                        consequatur debitis totam sit
+                        rem, sapiente ea delectus veritatis sed. Corrupti magni quam commodi error quae ratione,
+                        deserunt facere nemo? Maxime!</p>
+                    <p class="nome-autor">Lorem ipsum</p>
+                </div>
+            </div><!--w50-->
+            <div class="w50 left">
+                <h2 class="title">Servicos</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquam porro similique
+                            accusantium molestiae blanditiis delectus corporis aspernatur facere explicabo iusto ab eos
+                            qui, ducimus cum, inventore tempore eligendi voluptatum.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquam porro similique
+                            accusantium molestiae blanditiis delectus corporis aspernatur facere explicabo iusto ab eos
+                            qui, ducimus cum, inventore tempore eligendi voluptatum.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquam porro similique
+                            accusantium molestiae blanditiis delectus corporis aspernatur facere explicabo iusto ab eos
+                            qui, ducimus cum, inventore tempore eligendi voluptatum.</li>
+                    </ul>
+                </div>
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div>
+    </section>
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+
+</html>
